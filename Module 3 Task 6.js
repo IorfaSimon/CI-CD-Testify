548dec0055872b3f7530267c27b1ae72e7448fc0
Iorfa Ndernen Simon <126571948+IorfaSimon@users.noreply.github.com>
2023-06-28 02:09:21 +0100

    Add files via upload

---
+++ Module 3 Task 6.js
@@ -0,0 +1,192 @@
+const number = 6
+if('number % 2 == 0')
+console.log('number is even')
+else
+console.log('number is odd')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for(let oddNumber = 1;
+//     oddNumber <= 20;
+//     oddNumber += 2){
+//         console.log(oddNumber)
+//     }
+
+
+
+
+
+
+
+
+
+
+
+// const friendsArray = ['Ephraim', 'Moses', 'Nico', 'Philip', 'Emmy']
+
+// console.log(friendsArray)
+// // program to convert celsius to fahrenheit
+// // ask the celsius value to the user 
+// // 
+
+
+
+
+
+
+// function findArea(lenght,width){
+//     return lenght*width
+
+// }
+// let lenght=5
+// let width=3
+// const area=findArea(lenght,width)
+// console.log('areaOfrectangle')
+
+
+
+
+
+
+// // function to find area of arectangle
+// function findArea(lenght, width){
+//     return length * width}
+//     let lenght = 7;
+//     let width = 5;
+//     const Area=findArea(lenght, width)
+//     console.log('Area of a Rectangle: ' + area)
+
+
+// function findArea(lenght, width){
+    // return lenght * width;
+    // let lenght= 6
+    // let width=4
+    // const area=findArea(lenght,width)
+    // console.log('area of rectangle:' +area)
+
+
+
+
+
+// Function to find the area of a rectangle
+// function findArea(lenght, width) {
+//     return lenght * width;
+// }
+//     let lenght = 7;
+//     let width = 5;
+//     const area = findArea(lenght, width)
+//     console.log("Area of rectangle: " + area)
+
+ 
+
+
+
+
+// const sidesOftriangle = ('side1, side2, side3')
+// let side1 = 6
+// let side2 = 7
+// let side3 = 8
+// switch(sidesOftriangle){
+//     case(side1===side2 && side2===side3):
+//     console.log('triange is equilateral')
+//     break
+//     case(side1===side2 || side2===side3 || side1===side3):
+//     console.log('triangle is isoceles')
+//     break
+//     default:
+//         console.log('triangle is scalene')
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const sidesOftriangle = ('side2, side2,side3')
+// let side1 = 6
+// let side2 = 7
+// let side3 = 8
+// switch(sidesOftriangle){
+//     case(side1==side2 && side2==side3):
+// console.log('triangle is equilateral')
+// break
+// case(side1===side2||side2===side3||side1===side3):
+// console.log('triangle is isoceless')
+// break
+// default:
+// console.log('trianlge is Scalene')
+// }
+// const sidesOftriangle = ('side1, side2, side3')
+// let side1 = 6
+// let side2 = 7
+// let side3 = 8
+// switch(sidesOftriangle){
+//     case(side1===side2 && side2===side3):
+//     console.log('triangle is eqilateral')
+//     break
+//     case(side1===side2||side2===side3||side1===side3):
+//     console.log('triangle is isoceles')
+//     break
+//     default:
+//     console.log('triangle is scalene')
+// }
